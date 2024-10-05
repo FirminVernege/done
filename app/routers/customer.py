c81416847c76bc6b8c0f21080bfcee860bc46c3c
Firmin Vernege <ferbtography@gmail.com>
2024-10-05 12:59:20 -0400

    Added customer functions

--- app/routers/customer.py
+++ app/routers/customer.py
@@ -32,3 +32,38 @@ def create_customer(customer: schemas.CustomerCreate, db: Session = Depends(get_
     db.refresh(customer)
 
     return (customer)
+
+
+@router.get("/{id}")
+def get_customer(id: int, response: Response, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+
+    customer = db.query(models.Customer).filter(
+        models.Customer.created_by == current_user.id).filter(models.Customer.id == id).first()
+
+    if not customer:
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Customer with an id of {id} was not found")
+    return customer
+
+
+@router.delete("/{id}")
+def get_customer(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+
+    customer_query = db.query(models.Customer).filter(
+        models.Customer.created_by == current_user.id).filter(models.Customer.id == id)
+
+    customer = customer_query.first()
+
+    if customer == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Vehicle with an id of {id} does not exist")
+
+    if customer.created_by != customer.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"Not authorized to perform requested action.")
+    customer_query.delete(synchronize_session=False)
+
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
