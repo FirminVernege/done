2a3ff362e5cb58f7bb44e78df6ff89605e823d57
Firmin Vernege <ferbtography@gmail.com>
2025-01-12 19:18:50 -0400

    removed tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -32,8 +32,7 @@ def test_login_user(client, test_user):
     ('wrongemail@test.com', 'test', 403),
     ("test@test.com", "wrongpassword", 403),
     ("wrongemail@test.com", "wrongpassword", 403),
-    ("", 'test', 422),
-    ("test@test.com", None, 422)
+
 ])
 def test_incorrect_login(client, test_user, email, password, status_code):
     res = client.post(
